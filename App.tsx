c208b3f43b64515017295520b414ce49d73e346b
rushirami07 <82886004+rushirami07@users.noreply.github.com>
2021-07-18 00:52:11 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LiquidSwipe from "./LiquidSwipe";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LiquidSwipe />
   );
 }
 
